10b967f9937ed575f182e34c477dab155c0d14a3
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-06-02 12:26:36 -0300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,22 @@ using ImageCampus.ToolBox.Services;
 public class Player : Unit
 {
     private uint _life;
+
+    public void SetLife(uint life)
+    {
+        _life = life;
+    }
+
+    public void AddLife(uint life)
+    {
+        _life += life;
+    }
+
+    public void RemoveLife(uint life)
+    {
+        _life -= life;
+    }
+
     public void HandleMovement(Cell clickedCell)
     {
         RequestPath(clickedCell);
