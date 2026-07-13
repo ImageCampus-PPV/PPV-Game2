541aa0c9e02fd3ba3b29eb9a8490b29ade2ef451
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-07-13 19:54:16 -0300

    Update CellData.cs

--- Assets/Scripts/MapEditor/CellData.cs
+++ Assets/Scripts/MapEditor/CellData.cs
@@ -7,11 +7,13 @@ public struct CellData
     public string _initialState;
     public Vector2Int _coordinates;
     public string _spawnUnit;
+    public GameObject _assetToSpawn;
 
     public CellData(Vector2Int coordinates, string initialState)
     {
         _coordinates = coordinates;
         _initialState = initialState;
         _spawnUnit = "None";
+        _assetToSpawn = null;
     }
 }
