b31825fcb51770e994e19f71b44cfc1dff1e5eae
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-08-10 13:16:56 -0300

    Update Cell.cs

--- Assets/Scripts/Cell/Cell.cs
+++ Assets/Scripts/Cell/Cell.cs
@@ -49,11 +49,11 @@ public class Cell : BaseEntity
         Renderer renderer = GetComponent<Renderer>();
 
         _fsm.AddState<DefaultCell>();
-        _fsm.AddState<Unstable>(onEnterParameters: () => new object[] { renderer, 1 });
-        _fsm.AddState<Broken>(onEnterParameters: () => new object[] { renderer });
-        _fsm.AddState<Healing>(onEnterParameters: () => new object[] { renderer }, onTickParameters: () => new object[] { stander });
-        _fsm.AddState<Contagious>(onEnterParameters: () => new object[] { 10u, _coordinates, renderer }, onTickParameters: () => new object[] { stander });
-        _fsm.AddState<Infected>(onEnterParameters: () => new object[] { 10u, renderer }, onTickParameters: () => new object[] { stander });
+        _fsm.AddState<Unstable>(onEnterParameters: () => new object[] { this, renderer, 1 }, onExitParameters: () => new object[] { this });
+        _fsm.AddState<Broken>(onEnterParameters: () => new object[] { this, renderer }, onExitParameters: () => new object[] { this });
+        _fsm.AddState<Healing>(onEnterParameters: () => new object[] { this, renderer }, onTickParameters: () => new object[] { stander }, onExitParameters: () => new object[] { this });
+        _fsm.AddState<Contagious>(onEnterParameters: () => new object[] { this, 10u, _coordinates, renderer }, onTickParameters: () => new object[] { stander }, onExitParameters: () => new object[] { this });
+        _fsm.AddState<Infected>(onEnterParameters: () => new object[] { this, 10u, renderer }, onTickParameters: () => new object[] { stander }, onExitParameters: () => new object[] { this });
 
         _fsm.Transition(initialState);
     }
