62ba52e4d21d376b210a5a9612cae78e8abaf84a
Nicolas Leon <nicolasaleon05@gmail.com>
2026-08-31 14:39:09 -0300

    Create AttackAction

---
+++ Assets/Scripts/TurnActions/AttackAction.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+
+public class AttackAction : TurnAction, IAttackAction
+{
+    private readonly Unit _target;
+    private readonly uint _damage;
+
+    public AttackAction(Unit attacker, Unit target, uint damage, int totalTicks, int apCost = 0) : base(totalTicks, apCost)
+    {
+        _target = target;
+        _damage = damage;
+    }
+
+    public override IEnumerator Execute(Unit unit)
+    {
+        if (_target != null)
+        {
+            if (_target is Player player)
+                player.ReduceLife(_damage);
+        }
+
+        AdvanceTick();
+
+        yield return null;
+    }
+}
